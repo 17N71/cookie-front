cc6dd4600a91870fb732caef9176d234f66d1a83
17n71 <narek07012020@gmail.com>
2024-03-12 17:56:42 +0400

    add headers

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ function App() {
   const [myCookie, setmyCookie] = useState(() => Cookies.get("myCookie"));
   const submit = async e => {
     e.preventDefault();
-    await axios.post("https://cookie-test-amxb.onrender.com/auth");
+    await axios.post("https://cookie-test-amxb.onrender.com/auth", null, {
+      headers: {
+        "User-Agent": "insomnia/2023.5.8",
+        "Content-Type": "application/x-www-form-urlencoded",
+        content: "value",
+      },
+    });
     setmyCookie(Cookies.get("myCookie"));
   };
   return (
